ba1b326e2a593ace3d657765c74d46538b542ebc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-12 17:21:06 +0900

    feat: write sequelite test code

---
+++ todolist/src/db/test.rs
@@ -0,0 +1,37 @@
+use crate::todo::Todo;
+use sequelite::connection::Executable;
+use sequelite::model::query::ColumnQueryFilterImpl;
+use sequelite::model::ModelExt;
+use sequelite::prelude::Connection;
+
+pub fn main() {
+    // Create new database connection
+    let mut conn = Connection::new("dev.db").unwrap();
+
+    // Ensure database schema is up to date
+    conn.register::<Todo>().unwrap();
+    conn.migrate();
+
+    // Create a new todos
+    conn.insert(&[
+        Todo {
+            id: None,
+            content: "asd".to_string(),
+            done: false,
+        },
+        Todo {
+            id: None,
+            content: "qwe".to_string(),
+            done: false,
+        },
+    ]);
+
+    // Get all todos whose content is "John"
+    let todos = Todo::select()
+        .filter(Todo::content.eq(&"asd"))
+        .exec(&conn)
+        .unwrap();
+
+    // Print all todos
+    println!("{:#?}", todos);
+}
