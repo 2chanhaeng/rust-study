0aca26bc58d881bf534926d96ef4a9be3e40472f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-12 18:55:59 +0900

    feat: add add command

--- todolist/src/main.rs
+++ todolist/src/main.rs
@@ -14,6 +14,21 @@ fn main() {
     let args = Cli::parse();
     let conn = init();
     match args.command.as_str() {
+        "add" => {
+            let result = conn.insert(&[Todo {
+                id: None,
+                content: args.args.join(" "),
+                done: false,
+            }]);
+            let todo = match result {
+                Ok(id) => {
+                    let todo = Todo::select().with_id(id).exec(&conn).unwrap();
+                    format!("{:?}", todo)
+                }
+                _ => "Uh oh, something is wrong!".to_string(),
+            };
+            println!("{}", todo)
+        }
         "list" => {
             let todos = Todo::select().exec(&conn).unwrap();
             let todolist = todos
