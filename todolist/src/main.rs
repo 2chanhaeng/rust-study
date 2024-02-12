c675f1112560fa9dfb076049e487c26d4a4b0fa7
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-12 18:55:41 +0900

    feat: add list command

--- todolist/src/main.rs
+++ todolist/src/main.rs
@@ -1,6 +1,30 @@
 extern crate todolist;
+use std::fmt::format;
+
 use clap::Parser;
+use sequelite::model::query::ColumnQueryFilterImpl;
+use todolist::cli::main::Cli;
+use todolist::db::init::init;
+use todolist::todo::Todo;
+
+use sequelite::connection::Executable;
+use sequelite::model::ModelExt;
 
 fn main() {
     let args = Cli::parse();
+    let conn = init();
+    match args.command.as_str() {
+        "list" => {
+            let todos = Todo::select().exec(&conn).unwrap();
+            let todolist = todos
+                .iter()
+                .map(|todo: &Todo| {
+                    let done = if todo.done { "x" } else { " " };
+                    format(format_args!("[{}]\t{:?}\t{}", done, todo.id, todo.content))
+                })
+                .collect::<Vec<String>>()
+                .join("\n");
+            println!("{}", todolist)
+        }
+    }
 }
