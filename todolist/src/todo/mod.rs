7b250aabe663c6992b6bb7045838980637d00020
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-12 17:20:24 +0900

    feat: derive Todo struct as a Model

--- todolist/src/todo/mod.rs
+++ todolist/src/todo/mod.rs
@@ -1,6 +1,9 @@
-#[derive(Debug)]
+use sequelite::prelude::Model;
+
+#[derive(Debug, Model)]
 pub struct Todo {
-    pub id: i32,
+    pub id: Option<i32>,
     pub content: String,
+    #[default_value(&false)]
     pub done: bool,
 }
