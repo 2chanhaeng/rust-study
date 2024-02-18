908eec5cc21986b8847c81eaaf495ed25523dd81
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-18 17:47:22 +0900

    refac: remove FnBox

--- hello-server/src/worker/mod.rs
+++ hello-server/src/worker/mod.rs
@@ -1,17 +1,7 @@
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
-pub trait FnBox {
-    fn call_box(self: Box<Self>);
-}
-
-impl<F: FnOnce()> FnBox for F {
-    fn call_box(self: Box<F>) {
-        (*self)()
-    }
-}
-
-pub type Job = Box<dyn FnBox + Send + 'static>;
+pub type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct Worker {
     pub id: usize,
@@ -23,7 +13,7 @@ impl Worker {
         let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
             println!("Worker {} got a job; executing.", id);
-            job.call_box();
+            job();
         });
         Worker { id, thread }
     }
