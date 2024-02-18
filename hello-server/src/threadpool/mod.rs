5c46ebb1dbbe65862b43dadc9d8e5f786285070f
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-18 17:57:57 +0900

    fix: drop sender before join worker thread

--- hello-server/src/threadpool/mod.rs
+++ hello-server/src/threadpool/mod.rs
@@ -3,7 +3,7 @@ use std::sync::{mpsc, Arc, Mutex};
 
 pub struct ThreadPool {
     pub workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
@@ -16,23 +16,27 @@ impl ThreadPool {
         let workers = (0..size)
             .map(|id| Worker::new(id, Arc::clone(&receiver)))
             .collect::<Vec<Worker>>();
-        ThreadPool { workers, sender }
+        ThreadPool { workers, Some(sender) }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
-            worker.thread.join().unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
